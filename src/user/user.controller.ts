7dd9dd69eb20f63620577569b4cb70503eb7c606
ivanilsonAlmeida <ivan.almeidagomes@gmail.com>
2024-09-18 19:41:49 -0300

    wip including roles

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,40 +1,44 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { User } from './model/user.model';
-import { Employee } from '../employee/model/employee.model';
 import { UserService } from './user.service';
-import { MethodEnum } from './enum/method.enum';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { RoleEnum } from 'src/auth/roles/enum/role.enum';
+import { Roles } from 'src/auth/roles/role.decorator';
 
 @Controller('user')
 export class UserController {
 
   constructor(private readonly service: UserService) {}
 
-  @UseGuards(AuthGuard)
   @Post('/')
+  @Roles(RoleEnum.USER)
   public createUser(@Body() user: User) {    
     return this.service.create(user);
   }
 
   @UseGuards(AuthGuard)
+  @Roles(RoleEnum.ADMIN)
   @Delete(':email')
   public deleteUser(@Param('email') email: string) {
     return this.service.delete(email);
   }
 
   @UseGuards(AuthGuard)
+  @Roles(RoleEnum.ADMIN)
   @Put(':email')
   public updateUser(@Param('email') email: string, @Body() user: User) {
     return this.service.update(email, user);
   }
 
   @UseGuards(AuthGuard)
+  @Roles(RoleEnum.USER)
   @Get('/users')
   public listUser() {
     return this.service.findAll();
   }
 
   @UseGuards(AuthGuard)
+  @Roles(RoleEnum.USER)
   @Get(':email')
   public getUser(@Param('email') email: string) {
     return this.service.find(email);
