a2ef7cb3fa9991d5576c1e97e56f8557600a7534
ivanilsonAlmeida <ivan.almeidagomes@gmail.com>
2024-09-19 22:10:21 -0300

    wip roles

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Delete, Get, NotImplementedException, Param, Post, Pu
 import { EmployeeService } from './employee.service';
 import { Employee } from './model/employee.model';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { Roles } from 'src/auth/roles/role.decorator';
+import { RoleEnum } from 'src/auth/roles/enum/role.enum';
 
 @Controller('employee')
 export class EmployeeController {
@@ -9,18 +11,21 @@ export class EmployeeController {
   constructor(private readonly service: EmployeeService) {}
 
   @UseGuards(AuthGuard)
+  @Roles(RoleEnum.USER)
   @Post('/')
   public createEmployee(@Body() employee: Employee) {
     return this.service.create(employee);
   }
 
   @UseGuards(AuthGuard)
+  @Roles(RoleEnum.ADMIN)
   @Put(':registration')
   public updateEmployee(@Param('registration') registration: number, @Body() employee: Employee) {
     return this.service.update(registration, employee);
   }
 
   @UseGuards(AuthGuard)
+  @Roles(RoleEnum.ADMIN)
   @Delete(':registration')
   public deleteEmployee(@Param('registration') registration: number) {
     return this.service.delete(registration);
