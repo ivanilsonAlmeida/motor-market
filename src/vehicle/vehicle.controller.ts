54ff1349f5a25f63ce683027b10061526b74ab08
ivanilsonAlmeida <ivan.almeidagomes@gmail.com>
2024-09-27 17:18:41 -0300

    put roles and guards for vehicle

--- src/vehicle/vehicle.controller.ts
+++ src/vehicle/vehicle.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Get, NotImplementedException, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, NotImplementedException, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { VehicleService } from './vehicle.service';
 import { Vehicle } from './model/vehicle.model';
+import { Roles } from 'src/auth/roles/role.decorator';
+import { RoleEnum } from 'src/auth/roles/enum/role.enum';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('vehicle')
 export class VehicleController {
@@ -10,21 +13,29 @@ export class VehicleController {
   ) {}
 
   @Post('/')
+  @Roles(RoleEnum.USER)
+  @UseGuards(AuthGuard)
   public createVehicle(@Body() vehicle: Vehicle) {
     return this.service.create(vehicle);
   }
   
   @Put(':chassi')
+  @Roles(RoleEnum.USER)
+  @UseGuards(AuthGuard)
   public updateVehicle(@Param('chassi') chassi: string, @Body() vehicle: Vehicle) {
     return this.service.update(chassi, vehicle);
   }
 
   @Get('vehicles')
+  @Roles(RoleEnum.USER)
+  @UseGuards(AuthGuard)
   public listVehicle() {
     return this.service.findAll();
   }
 
   @Get(':chassi')
+  @Roles(RoleEnum.USER)
+  @UseGuards(AuthGuard)
   public getVehicle(@Param('chassi') chassi: string) {
     return this.service.find(chassi);
   }
