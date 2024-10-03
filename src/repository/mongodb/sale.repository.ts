9755165b5a5b50e97a9da81faec2ae7e404ed332
ivanilsonAlmeida <ivan.almeidagomes@gmail.com>
2024-10-03 17:52:07 -0300

    adjust repository from sale

--- src/repository/mongodb/sale.repository.ts
+++ src/repository/mongodb/sale.repository.ts
@@ -2,31 +2,59 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Sale } from "src/sale/model/sale.model";
 import { Repository } from "./interface/repository.interface";
+import { Injectable, NotImplementedException } from "@nestjs/common";
 
+@Injectable()
 export class SaleRepository implements Repository<Sale>{
 
   constructor(
     @InjectModel(Sale.name) private readonly saleModel: Model<Sale>
   ) {}
 
-  create(sale: Sale) {
-    throw new Error("Method not implemented.");
+  public async create(sale: Sale) {
+    try {
+      return this.saleModel.create(sale); 
+    } catch (error) {
+      console.error(`An error occurred in the repository: ${error}`);
+      return error?.data;
+    }
   }
 
-  update(sale: Sale) {
-    throw new Error("Method not implemented.");
+  public async update(sale: Sale) {
+    try {
+      return this.saleModel.updateOne({
+        registration: sale.registration,
+        nameClient: sale.nameClient,
+        totalPrice: sale.totalPrice,
+        payment: sale.payment,
+        vehicle: sale.vehicle
+      }); 
+    } catch (error) {
+      console.error(`An error occurred in the repository: ${error}`);
+      return error?.data;
+    }
   }
 
-  findOne(registration: number) {
-    throw new Error("Method not implemented.");
+  public async findOne(registration: number) {
+    try {
+      return this.saleModel.findOne({registration}); 
+    } catch (error) {
+      console.error(`An error occurred in the repository: ${error}`);
+      return error?.data;
+    }
   }
 
-  findAll() {
-    throw new Error("Method not implemented.");
+  public async findAll() {
+    try {
+      return this.saleModel.find(); 
+    } catch (error) {
+      console.error(`An error occurred in the repository: ${error}`);
+      return error?.data;
+    }
   }
 
-  delete(t: string | number) {
-    throw new Error("Method not implemented.");
+  public async delete(t: string | number) {
+    throw NotImplementedException;
   }
 
 
--- src/repository/repository.module.ts
+++ src/repository/repository.module.ts
@@ -9,15 +9,29 @@ import { EmployeeSchema } from './mongodb/schema/employee.schema';
 import { VehicleRepository } from './mongodb/vehicle.repository';
 import { VehicleSchema } from './mongodb/schema/vehicle.schema';
 import { Vehicle } from 'src/vehicle/model/vehicle.model';
+import { SaleSchema } from './mongodb/schema/sale.schema';
+import { Sale } from 'src/sale/model/sale.model';
+import { SaleRepository } from './mongodb/sale.repository';
 
 
 @Module({
-  providers: [UserRepository, EmployeeRepository, VehicleRepository],
-  exports: [UserRepository, EmployeeRepository, VehicleRepository],
+  providers: [
+    UserRepository, 
+    EmployeeRepository, 
+    VehicleRepository,
+    SaleRepository
+  ],
+  exports: [
+    UserRepository, 
+    EmployeeRepository, 
+    VehicleRepository,
+    SaleRepository
+  ],
   imports: [
     MongooseModule.forFeature([{name: User.name, schema: UserSchema}]),
     MongooseModule.forFeature([{name: Employee.name, schema: EmployeeSchema}]),
-    MongooseModule.forFeature([{name: Vehicle.name, schema: VehicleSchema}])
+    MongooseModule.forFeature([{name: Vehicle.name, schema: VehicleSchema}]),
+    MongooseModule.forFeature([{name: Sale.name, schema: SaleSchema}])
   ]
 })
 export class RepositoryModule {}
